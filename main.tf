393ae08da330fe348f5917d8d8eccee9f3b2dc89
1Mill <erik.ekberg@outlook.com>
2020-07-03 13:18:57 -0700

    Migrate everything to remote state

--- main.tf
+++ main.tf
@@ -11,6 +11,23 @@ provider "heroku" {
 	version = "~> 2.5"
 }
 
+terraform {
+	required_version = "~> 0.12.26"
+
+	backend "s3" {
+		// profile = ENVIRONMENT AWS_PROFILE
+		// region = ENVIRONMENT AWS_DEFAULT_REGION
+
+		// access_key = ENVIRONMENT AWS_ACCESS_KEY_ID
+		// secret_key = ENVIRONMENT AWS_SECRET_ACCESS_KEY
+
+		bucket = "experiment-terraform-state"
+		dynamodb_table = "experiment-terraform-state-locks"
+		encrypt = true
+		key = "global/s3/terraform.tfstate"
+	}
+}
+
 // https://devcenter.heroku.com/articles/using-terraform-with-heroku#deploying-code-to-an-app
 // Create application in heroku
 resource "heroku_app" "default" {
