765a982e1cb3f696bd8af92b5f14b90df54c1a5f
1Mill <erik.ekberg@outlook.com>
2020-07-02 21:48:10 -0700

    Create slug

--- main.tf
+++ main.tf
@@ -15,3 +15,13 @@ resource "heroku_app" "default" {
 	name = "my-getting-started-node-app"
 	region = "us"
 }
+
+resource "heroku_slug" "default" {
+	app = heroku_app.default.id
+	buildpack_provided_description = "Node/Express"
+	file_url = "https://github.com/1Mill/node-js-getting-started/archive/v0.0.1.tar.gz"
+
+	process_types = {
+		web = "npm run start"
+	}
+}
